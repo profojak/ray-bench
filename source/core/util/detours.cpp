e0a4458add3f002aa300143a0203470a8850257a
Jakub Profota <245464941+profojak@users.noreply.github.com>
2026-02-08 17:20:13 +0100

    💎 Launch target application using Detours
    Co-authored-by: Jakub Profota <profojak@fel.cvut.cz>

--- source/core/util/detours.cpp
+++ source/core/util/detours.cpp
@@ -14,8 +14,96 @@ module;
 
 export module RayBench.Util:Detours;
 
+import std;
+
 namespace raybench::util
 {
+
+// ----------------------------------------------------------------------------
+
+/// @brief Paths related to a process to launch and inject into
+struct ProcessPaths
+{
+    ///< Path to the application executable
+    std::string app_path;
+    ///< Command line arguments for the application
+    std::string arguments;
+    ///< Working directory for the application
+    std::string app_directory;
+};
+
+// ----------------------------------------------------------------------------
+
+/// @brief Parse the full command line of a process to extract its paths
+/// 
+/// @param full_command Full command line of the process, including the
+///                     executable path and its arguments
+/// @return Optional containing the parsed paths if successful,
+///         `std::nullopt` otherwise
+export [[nodiscard]] std::optional<ProcessPaths> GetProcessPaths (std::string_view full_command)
+{
+    using namespace std::string_view_literals;
+
+    const auto exe_pos = full_command.find (".exe"sv);
+    if (exe_pos == std::string_view::npos)
+    {
+        return std::nullopt;
+    }
+
+    const size_t args_start = exe_pos + 4;
+    std::filesystem::path path_view {full_command.substr (0, args_start)};
+
+    return ProcessPaths {
+        .app_path = path_view.string (),
+        .arguments = std::format ("\"{}\"{}", path_view.string (), full_command.substr (args_start)),
+        .app_directory = path_view.parent_path ().string ()
+    };
+}
+
+// ----------------------------------------------------------------------------
+
+/// @brief Launch a process and inject dynamic-link libraries into it using
+///        Detours
+/// 
+/// @param process_paths Paths related to the process to launch and inject into
+/// @param startup_info Startup information for the application
+/// @param process_info Process information for the application
+/// @param dlls_count Number of dynamic-link libraries to inject
+/// @param dlls Array of paths to the dynamic-link libraries
+/// @return True if the process was launched and the libraries were injected
+///         successfully, false otherwise
+export bool LaunchAndInject (ProcessPaths process_paths,
+                             DWORD dlls_count,
+                             LPCSTR* dlls)
+{
+    STARTUPINFOA startup_info = {};
+    startup_info.cb = sizeof (startup_info);
+    PROCESS_INFORMATION process_info = {};
+
+    bool result = DetourCreateProcessWithDllsA (process_paths.app_path.data (),
+                                                process_paths.arguments.data (),
+                                                nullptr,
+                                                nullptr,
+                                                TRUE,
+                                                CREATE_SUSPENDED,
+                                                nullptr,
+                                                process_paths.app_directory.data (),
+                                                &startup_info,
+                                                &process_info,
+                                                dlls_count,
+                                                dlls,
+                                                nullptr);
+
+    if (result == false)
+    {
+        return false;
+    }
+
+    // At this point, injected dynamic-link libraries have run their `DllMain`.
+    // Resume the main thread of the process to start execution.
+    ResumeThread (process_info.hThread);
+    return true;
+}
 
 // ----------------------------------------------------------------------------
 
@@ -67,8 +155,6 @@ export bool WINAPI HookAPICall (PVOID* real_fn, PVOID hook_fn)
 /// @return True if the unhook was successful, false otherwise
 export bool WINAPI UnhookAPICall (PVOID* real_fn, PVOID hook_fn)
 {
-    InitializeDetours ();
-
     DetourTransactionBegin ();
     DetourUpdateThread (GetCurrentThread ());
 
--- source/tools/inject/main.cpp
+++ source/tools/inject/main.cpp
@@ -29,12 +29,12 @@ static void PrintHelp (const std::string_view path)
     }
 
     RAYBENCH_WRITE_CONSOLE ("{}: inject tool\n\n"
-        "Usage:\n"
-        "  {} [-h|--help] <target>\n\n"
-        "Arguments:\n"
-        "  <target>            Target application to inject into\n\n"
-        "Options:\n"
-        "  -h, --help          Show this help message and exit\n", name, name);
+                            "Usage:\n"
+                            "  {} [-h|--help] <target>\n\n"
+                            "Arguments:\n"
+                            "  <target>            Target application to inject into\n\n"
+                            "Options:\n"
+                            "  -h, --help          Show this help message and exit\n", name, name);
 }
 
 // ----------------------------------------------------------------------------
@@ -53,11 +53,22 @@ int main (int argc, const char** argv)
         args.IsOptionSet ("--help") || args.IsOptionSet ("-h"))
     {
         PrintHelp (argv[0]);
+        raybench::util::Log::Release ();
         return 1;
     }
- 
-    raybench::util::Log::Release ();
 
+    auto process_paths = raybench::util::GetProcessPaths (args.GetPositionalArguments ().front ());
+    if (process_paths.has_value() == false)
+    {
+        RAYBENCH_LOG_ERROR ("No process found matching the specified target: {}",
+                            args.GetPositionalArguments ().front ());
+        raybench::util::Log::Release ();
+        return 1;
+    }
+
+    raybench::util::LaunchAndInject (process_paths.value(), 0, nullptr);
+
+    raybench::util::Log::Release ();
     return 0;
 }
 
