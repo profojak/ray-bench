4d7f7b81c4da0043355df95cb5af088b0502ea9a
Jakub Profota <245464941+profojak@users.noreply.github.com>
2026-03-11 17:00:08 +0100

    💎 Prepare module for API hooks
    Co-authored-by: Jakub Profota <profojak@fel.cvut.cz>

---
+++ source/hooks/hook/directx12_wrap.cpp
@@ -0,0 +1,21 @@
+// ============================================================================
+
+/// @brief DirectX 12 hooks
+
+module;
+
+#include "util/log.h"
+
+export module RayBench.Hook:DirectX12;
+
+import std;
+import RayBench.Util;
+
+namespace raybench::hook
+{
+
+
+
+}
+
+// ----------------------------------------------------------------------------
